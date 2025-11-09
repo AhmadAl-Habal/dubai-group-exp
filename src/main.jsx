21559a2b5c44887cf5c8ff281cde907a53dbb219
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2025-11-09 15:07:17 +0300

    Add analytics feature

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
+import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")).render(
   process.env.NODE_ENV === "development" ? (
     <StrictMode>
       <App />
+          <Analytics />
     </StrictMode>
   ) : (
-    <App />
+    <><App />
+        <Analytics /></>
   )
 );
   
